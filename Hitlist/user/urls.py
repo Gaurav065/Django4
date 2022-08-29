3f556d968c81dc928ebabcbd2450f851676656af
gaurav652002 <warrior.prince652002@gmail.com>
2022-08-29 09:48:34 +0530

    changes

--- Hitlist/user/urls.py
+++ Hitlist/user/urls.py
@@ -2,5 +2,6 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('',views.user_log, name='Login')
+    path('',views.user_log, name='Login'),
+    path('home/', views.home, name='Home'),
 ]
