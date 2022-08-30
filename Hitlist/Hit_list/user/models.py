44663829d7aeb410f55d0689272b40c4e416a100
gaurav652002 <warrior.prince652002@gmail.com>
2022-08-30 15:34:16 +0530

    changes

--- Hitlist/Hit_list/user/models.py
+++ Hitlist/Hit_list/user/models.py
@@ -1,4 +1,7 @@
 from socket import fromshare
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class user_cust(models.Model):
+    user = models.
