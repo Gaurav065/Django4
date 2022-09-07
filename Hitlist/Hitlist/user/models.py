e64700ce0e922c8ac53aecffdebe6ee17cff3137
gaurav652002 <warrior.prince652002@gmail.com>
2022-09-07 23:57:29 +0530

    changes

--- Hitlist/Hitlist/user/models.py
+++ Hitlist/Hitlist/user/models.py
@@ -1,3 +1,11 @@
+from email.policy import default
+from unicodedata import name
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Hitter(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    contact = models.CharField(max_length=13)
+    about = models.TextField(default="I am a Hitter")
+    avatar = models.ImageField(uplaod_to='user_avatars/',default='user_avatars/hitter.png')
