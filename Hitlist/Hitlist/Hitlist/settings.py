ff3e05088b04a04ecfef82f7fc6f75622461eaaf
gaurav652002 <warrior.prince652002@gmail.com>
2022-09-07 22:13:07 +0530

    changes

--- Hitlist/Hitlist/Hitlist/settings.py
+++ Hitlist/Hitlist/Hitlist/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -121,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_ROOT = '/media/'
-MEDIA_URL =  os.path.join(BASE_DIR, 'media')
+MEDIA_URL =  os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
