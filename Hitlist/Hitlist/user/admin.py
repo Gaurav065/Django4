ef9b2da1b74ea56c8d277bbeefa3172b483e48d4
gaurav652002 <warrior.prince652002@gmail.com>
2022-09-08 00:46:31 +0530

    Changes and addition of hitter which is the user profile

--- Hitlist/Hitlist/user/admin.py
+++ Hitlist/Hitlist/user/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Hitter
 
+
+admin.site.register(Hitter)
 # Register your models here.
--- Hitlist/Hitlist/user/models.py
+++ Hitlist/Hitlist/user/models.py
@@ -8,4 +8,16 @@ class Hitter(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     contact = models.CharField(max_length=13)
     about = models.TextField(default="I am a Hitter")
-    avatar = models.ImageField(uplaod_to='user_avatars/',default='user_avatars/hitter.png')
+    avatar = models.ImageField(upload_to='user_avatars/',default='user_avatars/hitter.png')
+    created = models.DateTimeField(auto_now_add=True)
+    edited = models.DateTimeField(auto_now=True)
+
+    def full_name(self):
+        return (self.user.first_name +" " +self.user.last_name)
+
+
+    def __str__(self):
+        return self.user.username
+
+
+    
