4e6843ed9432114ac009ac4cbb694bd27a8fa3b4
Gaurav Patel <warrior.prince652002@gmail.com>
2022-09-26 15:38:31 +0530

    new changes with class based views

--- Hitlist/hitlist/user/models.py
+++ Hitlist/hitlist/user/models.py
@@ -1,32 +1,20 @@
 from django.db import models
-from django.utils import timezone
+from django.contrib.auth.models import User
 
-
-class Hitter_profile(models.Model):
-
-    name = models.CharField(max_length=200, null=True)
-    phone = models.CharField(max_length=15, null=True)
-    email = models.EmailField()
-    date_created = models.DateTimeField(auto_now_add=True, null=True)
-    avatar = models.ImageField(default='media/')
-    user_hits = models.ForeignKey('Hits',on_delete=models.CASCADE)
-   
-    
-
-    def __str__(self):
-        return self.name
 
 class Hits(models.Model):
-    user_name= models.ForeignKey(Hitter_profile, on_delete=models.CASCADE)
-    # user_avatar = models.ForeignKey(Hitter_profile.avatar, on_delete=models.CASCADE)
-
-    hit = models.CharField(max_length=100)
-    hit_brief = models.TextField(max_length=1000)
+    user_name= models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    
+    hit = models.CharField(max_length=200)
+    hit_brief = models.TextField(max_length=1000,null=True, blank=True)
     hit_link  = models.URLField(null=True)
     hit_img = models.ImageField(null=True,default='media/upload_images/hit.png')
     hit_file  = models.FileField(null=True)
-    hit_start_date = models.DateField( auto_now_add=True)
-    hit_end_date = models.DateField( auto_now_add=True)
+    hit_start_date = models.DateTimeField( auto_now_add=True)
+    hit_confirmation  = models.BooleanField(default=False)
 
     def __str__(self):
         return self.hit
+
+    class Meta:
+        arrangement = ['hit_confirmation']
