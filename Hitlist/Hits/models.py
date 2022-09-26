f7537d0e44167edccbb462d98f78d87ca7bfee1c
Gaurav Patel <warrior.prince652002@gmail.com>
2022-09-26 20:36:35 +0530

    changes

--- Hitlist/Hits/models.py
+++ Hitlist/Hits/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+class Hitter(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    hit = models.CharField(max_length=150, null=True, blank= True)
+    hit_brief = models.CharField(max_length=300, null=True, blank=True)
+    hit_status = models.BooleanField(default=False)
+    hit_history = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.hit
