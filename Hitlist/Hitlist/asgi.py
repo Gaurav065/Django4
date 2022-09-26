56b312ae13f4ff5717dde3d7cb1cfd5ac2904b34
Gaurav Patel <warrior.prince652002@gmail.com>
2022-09-26 20:18:12 +0530

    changes and new creation

--- Hitlist/Hitlist/asgi.py
+++ Hitlist/Hitlist/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for hitlist project.
+ASGI config for Hitlist project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hitlist.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Hitlist.settings')
 
 application = get_asgi_application()
--- Hitlist/Hitlist/urls.py
+++ Hitlist/Hitlist/urls.py
@@ -1,4 +1,4 @@
-"""hitlist URL Configuration
+"""Hitlist URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -20,6 +20,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('user.urls')),
-    
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
