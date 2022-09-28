b4d072140553dea77cb0e4339e96eddaba730c57
Gaurav Patel <warrior.prince652002@gmail.com>
2022-09-28 20:40:31 +0530

    changes in the models, implementing the absetractuser before the abstractbaseuser

--- Hitlist/Hitlist/settings.py
+++ Hitlist/Hitlist/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,3 +126,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+USER_AUTH_MODELS = Hits.Hitter
--- Hitlist/Hits/models.py
+++ Hitlist/Hits/models.py
@@ -1,16 +1,70 @@
+from locale import normalize
+from optparse import Values
 from django.db import models
-from django.contrib.auth.models import User
-
-
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth import get_user_model
+from django.utils import timezone
 
 
 class Hitter(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    hit = models.CharField(max_length=150, null=True, blank= True)
-    hit_brief = models.CharField(max_length=300, null=True, blank=True)
-    hit_status = models.BooleanField(default=False)
-    hit_history = models.DateTimeField(auto_now_add=True)
+    
 
 
-    def __str__(self):
-        return self.hit
+# class Hitter_base(models.Model):
+#     use_in_migrations = True
+
+#     def create_user_base(self, name, phone, email, password, **extra_fields):
+#         values = [email, name, phone]
+#         field_value_map = dict(zip(self.model.REQUIRED_FIELDS, values))
+#         for field_name, value in field_value_map.items():
+#             if not value:
+#                 raise ValueError('The {} value must be set'.format(field_name))
+
+#         email =self.normalize_email(email)
+#         user = self.model(email=email, name=name, phone=phone, **extra_fields)
+#         user.set_password(password)
+#         user.save(using=self._db)
+#         return user
+
+
+#     def create_hitter(self, name, phone, email, password=None, **extra_fields):
+#         extra_fields.setdefault('is_staff',False)
+#         extra_fields.setdefault('is_superuser', False)
+#         return self.create_user_base(name, phone,email,password, **extra_fields)
+
+
+#     def create_superuser(self, name,phone, email,password, **extra_fields):
+#         extra_fields.setdefault('is_staff',True)
+#         extra_fields.setdefault('is_superuser', True)
+
+
+#         if extra_fields.get('is_staff') is not True:
+#             raise ValueError(" Superuser must have is_staff=True")
+#         if extra_fields.get('is_superuser') is not True:
+#             raise ValueError('Superuser must have is_superuser=True')
+
+#         return self.create_user_base(name, phone,email,password, **extra_fields)
+
+
+# class Hitter(AbstractBaseUser, PermissionsMixin):
+#     email=models.EmailField(unique=True)
+#     name= models.CharField(max_length=150)
+#     phone = models.CharField(max_length=50)
+#     birth_date = models.DateField(blank=FALSE, null=True)
+#     avatar = models.ImageField(blank=True, null=True)
+#     is_staff = models.BooleanField(default=False)
+#     is_active = models.BooleanField(default=True)
+#     date_joined = models.DateTimeField(default=timezone.now)
+#     last_login = models.DateTimeField(null=True)
+
+
+#     obj = Hitter_base()
+
+#     USERNAME_FIELDS = 'email'
+#     REQUIRED_FIELDS = ['name', 'phone']
+
+#     def get_full_name(self):
+#         return self.name
+    
+#     def get_first_name(self):
+#         return self.name.split()[0]
