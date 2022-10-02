ee15a55e1cbe52854515892a855995a157a630df
Gaurav Patel <warrior.prince652002@gmail.com>
2022-10-02 16:38:26 +0530

    finalizing the to do list  application with full login and logut , single user view and proper rerouting of pages and urls

--- Hitlist/core/urls.py
+++ Hitlist/core/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path,include
-from .views import Hitlist
+from .views import Hitlist,HitDetails, HitCreate, HitUpdate, HitTerminate, HitterLoginView
+from django.contrib.auth.views import LogoutView as HitterLogoutView
 
 urlpatterns = [
+
+    path("hitter_login/", HitterLoginView.as_view(), name='Hiiter_DiveIn'),
+    path("hitter_logout/", HitterLogoutView.as_view(), name="Hitter_DiveOut"),
     path('', Hitlist.as_view(), name='Hitlist'),
+    path('hit/<int:pk>/', HitDetails.as_view(),name='hit'),
+    path('hit_init/', HitCreate.as_view(), name='Hit_init'),
+    path("hit_update/<int:pk>/", HitUpdate.as_view(), name="Hit_update"),
+    path("hit_terminate/<int:pk>/", HitTerminate.as_view(), name="Hit_terminate"),
+    
 ]
--- Hitlist/core/views.py
+++ Hitlist/core/views.py
@@ -1,7 +1,58 @@
+from audioop import reverse
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+from django.contrib.auth.views import LoginView,LogoutView
+
+from django.urls import reverse_lazy
 from .models import hitter
+
+
+
+class HitterLoginView(LoginView):
+    template_name= 'core/Hitter_DiveIn.html'
+    fields = '__all__'
+    redirect_authenticated_user=True
+
+    def get_success_url(self):
+        return reverse_lazy('Hitlist')
+
+class HitterLogoutView(LogoutView):
+    template_name='core/Hitter_DiveOut.html'
+    fields = '__all__'
+    re
 
 class Hitlist(ListView):
     model = hitter 
+    context_object_name= 'Hits'
+    ordering=['date_created']
+    
+class HitDetails(DetailView):
+    model = hitter
+    context_object_name= 'h'
+    template_name= 'core/hits.html'
+
+
+class HitCreate(CreateView):
+    model = hitter
+    template_name='core/hit_init.html'
+
+    fields= '__all__'
+    success_url=reverse_lazy('Hitlist')
+
+class HitUpdate(UpdateView):
+    model = hitter
+    template_name='core/hit_updation.html'
+
+    fields = '__all__'
+    success_url= reverse_lazy('Hitlist')
+
+class HitTerminate(DeleteView):
+    model = hitter
+    template_name='core/hit_terminate.html'
+
+    context_object_name='h'
+    success_url= reverse_lazy('Hitlist')
     
