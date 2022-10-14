5962797c69e404abfe872c97842081cfa8d21ad5
Gaurav Patel <warrior.prince652002@gmail.com>
2022-10-14 12:49:01 +0530

    changes

--- Hitlist/core/urls.py
+++ Hitlist/core/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path,include
-from .views import Hitlist,HitDetails, HitCreate, HitUpdate, HitTerminate, HitterLoginView
+from .views import Hitlist,HitDetails, HitCreate, HitUpdate, HitTerminate, HitterLoginView, HitterRegister
 from django.contrib.auth.views import LogoutView 
 
 urlpatterns = [
 
     path("hitter_divein/", HitterLoginView.as_view(), name='Hitter_DiveIn'),
     path("hitter_diveout/", LogoutView.as_view(next_page='Hitter_DiveIn'), name="Hitter_DiveOut"),
+    path("register/",HitterRegister.as_view(),name = "Register" ),
     path('', Hitlist.as_view(), name='Hitlist'),
     path('hit/<int:pk>/', HitDetails.as_view(),name='hit'),
     path('hit_init/', HitCreate.as_view(), name='Hit_init'),
--- Hitlist/core/views.py
+++ Hitlist/core/views.py
@@ -1,12 +1,15 @@
 from audioop import reverse
+from attr import fields
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 
 from django.contrib.auth.views import LoginView,LogoutView
 
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 from django.urls import reverse_lazy
 from .models import hitter
@@ -22,11 +25,25 @@ class HitterLoginView(LoginView):
         return reverse_lazy('Hitlist')
 
 
+class HitterRegister(FormView):
+    template_name='core/register.html'
+    fields="__all__"
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url= reverse_lazy('Hitlist')
+    
+
+
+
 
 class Hitlist(LoginRequiredMixin,ListView):
     model = hitter 
     context_object_name= 'Hits'
     ordering=['date_created']
+    
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(**kwargs)
+        context 
     
 class HitDetails(LoginRequiredMixin,DetailView):
     model = hitter
